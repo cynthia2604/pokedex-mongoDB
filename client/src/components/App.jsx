753ae98e5b00b42815242414195a3b93baff4e81
“Cynthia <xin2604@gmail.com>
2021-12-19 15:29:18 -0800

    complete front end insert new pokemon

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,11 +7,19 @@ class App extends React.Component {
     super(props);
     this.state = {
       pokemonlist: [],
-      type: ''
+      type: '',
+      inserted: false,
+      inputname: '',
+      inputtype: '',
+      inputimg: ''
     }
     this.getAllPokemon = this.getAllPokemon.bind(this);
     this.handleTypeSelect = this.handleTypeSelect.bind(this);
     this.getType = this.getType.bind(this);
+    this.handleInsert = this.handleInsert.bind(this);
+    this.addPokemon = this.addPokemon.bind(this);
+    this.handleInsertChange = this.handleInsertChange.bind(this);
+    this.addPokemonRequest = this.addPokemonRequest.bind(this);
   }
 
   componentDidMount() {
@@ -29,7 +37,6 @@ class App extends React.Component {
       .catch((err) => console.error(err));
   }
 
-  // axios.get('/pokemonlist/type', {params: {type: this.state.type} })
   getType() {
     // axios get request to retrive specific type pokemons
     axios.get(`/pokemonlist/type/${this.state.type}`)
@@ -49,7 +56,58 @@ class App extends React.Component {
       type: e.target.value
     }, this.getType)
   }
+  handleInsertChange(e) {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  // if inserted is true, dislay insert form and send post request
+  addPokemon() {
+    if(this.state.inserted) {
+      return (
+        <form onSubmit={this.addPokemonRequest}>
+          <label>Insert New Pokemon</label>
+          <br></br>
+          <label>
+            Enter Name:
+            <input type="text" name="inputname" onChange={this.handleInsertChange}></input>
+          </label>
+          <br></br>
+          <label>
+            Enter Type:
+            <input type="text" name="inputtype" value={this.state.inputtype} onChange={this.handleInsertChange}></input>
+          </label>
+          <br></br>
+          <label>
+            Enter ImageUrl:
+            <input type="text" name="inputimg" value={this.state.inputimg} onChange={this.handleInsertChange}></input>
+          </label>
+          <br></br>
+          <button type="submit" value="Submit">Submit</button>
+        </form>
+      )
+    }
+  }
+  // display or hide add pokemon form
+  handleInsert() {
+    console.log('click insert');
+    this.setState({
+      inserted : !this.state.inserted
+    })
+  }
 
+  addPokemonRequest(e) {
+    e.preventDefault();
+    console.log(this.state.inputname, this.state.inputtype, this.state.inputimg);
+    //axios post request
+    axios.post('/pokemonlist/pokemons', {
+      name : this.state.inputname,
+      type : this.state.inputtype,
+      img: this.state.inputimg
+    })
+      .then(() => this.getAllPokemon())
+      .catch((err) => console.error(err))
+  }
   render() {
     return (
       <div>
@@ -71,7 +129,8 @@ class App extends React.Component {
           <option>Ghost</option>
           <option>Dragon</option>
         </select>
-        <button>INSERT</button>
+        <button onClick={this.handleInsert}>INSERT</button>
+        {this.addPokemon()}
         <PokemonList pokemons={this.state.pokemonlist} getAllPokemon={this.getAllPokemon}/>
       </div>
     </div>
